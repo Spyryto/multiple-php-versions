a34850a140766456c7f228c31d1370189b326644
Spyryto <danielezecchin@gmail.com>
2021-02-24 15:27:30 +0100

    Fixed generated2 script

--- cert/generate_certificate2.bat
+++ cert/generate_certificate2.bat
@@ -1,4 +1,4 @@
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes ^
-  -keyout %1.key -out %1.crt -subj "/CN=%1.%2" ^
+  -keyout %1.%2.key -out %1.%2.crt -subj "/CN=%1.%2" ^
   -addext "subjectAltName=DNS:%1.%2,IP:127.0.0.1"
 
